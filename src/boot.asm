b71861d8e9670a0fb8d298ec04e9fd2b8f0fbb80
Xiao-Ling-NCA <chengfuzhao@hotmail.com>
2026-05-06 23:38:21 +0000

    new file:   XiaoLingOS/boot/grub/grub.cfg 	new file:   makefile 	new file:   src/boot.asm 	new file:   src/kernel.c 	new file:   src/linker.lld 	new file:   src/stdlib/stdint.h 	new file:   src/stdlib/stdio.c 	new file:   src/stdlib/stdio.h

---
+++ src/boot.asm
@@ -0,0 +1,23 @@
+[bits 32]
+section .text
+global _start
+_start:
+    align 4
+    dd 0x1BADB002
+    dd 0x00000000
+
+    dd -(0x1BADB002 + 0x00000000)
+global start
+extern kmain
+start:
+    mov esp, stack_space
+    call kmain
+
+halt_kernel:
+    hlt
+    jmp $
+
+section .bss
+resb 8192 ;8kb stack
+
+stack_space:
